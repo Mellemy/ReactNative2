e76985b9244a8cf8bb8f75618cd6cf38a3f2fed9
Mellemy <emppuli2003@gmail.com>
2025-07-31 21:25:51 +0300

    Scrolling

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -9,13 +9,21 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.barBackground,
     flexDirection: 'row',
   },
-  // ...
+   scroll: {
+    flexDirection: 'row',
+  },
 });
 
 const AppBar = () => {
   return <View style={styles.container}>
+  <ScrollView horizontal style={styles.scroll}>
   <AppBarTab title="Repositories" to="/" />
   <AppBarTab title="Sign in" to="/signin" />
+  <AppBarTab title="Test 3" to="/ttest3" />
+  <AppBarTab title="Test 4" to="/test4" />
+  <AppBarTab title="Test 5" to="/test5" />
+  <AppBarTab title="Test 6" to="/test6" />
+  </ScrollView>
   </View>;
 };
 
