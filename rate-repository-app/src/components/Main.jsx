76f53f731422e0e4cdd60e00ec7600bc9253df77
Mellemy <emppuli2003@gmail.com>
2025-07-31 19:49:34 +0300

    10.6 base

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -2,6 +2,7 @@ import Constants from 'expo-constants';
 import { Text, StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import { Route, Routes, Navigate } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,7 +15,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
          <AppBar />
-        <RepositoryList />
+       <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
