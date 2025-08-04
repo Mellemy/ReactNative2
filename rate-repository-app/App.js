42355f51788bedb35b7dca88f6a3d74b84f88404
Mellemy <emppuli2003@gmail.com>
2025-08-04 23:10:48 +0300

    10.11 done

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,13 +1,15 @@
-import Main from './src/components/Main';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
 
+import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+   console.log(Constants.expoConfig);
   return (
     <>
     <NativeRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,25 +1,36 @@
 import { useState, useEffect } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      edges {
+        node {
+          id
+          fullName
+          description
+          language
+          forksCount
+          stargazersCount
+          ratingAverage
+          reviewCount
+          ownerAvatarUrl
+        }
+      }
+    }
+  }
+`;
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://10.0.2.2:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
+  return {
+    repositories: data?.repositories,
+    loading,
+    refetch,
   };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
 };
 
 export default useRepositories;
